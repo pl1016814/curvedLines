99dd52e87e36f7ed062e066993b6f56454191e30
pl1016814 <pl1016814@ahschool.com>
2026-02-11 09:46:24 -0500

    Update image source and processing parameters

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import cv2 as cv
 import numpy as np
 
 # SETUP
-img = cv.imread("CurvedLines2.jpeg")
-img = cv.resize(img, (300, 400), interpolation=cv.INTER_AREA)
+img = cv.imread("Screenshot2.png")
+img = cv.resize(img, (400, 200), interpolation=cv.INTER_AREA)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #gaussian blur as stated in documentation doesn't work for my soda can, idk why
-gray = cv.GaussianBlur(img, (7, 7), 1.5)
+gray = cv.GaussianBlur(img, (9, 9), 2.5)
 cannyProcess = cv.Canny(gray, 135, 145)
 
 centerPoints = []
@@ -20,9 +20,10 @@ for i in range(cannyProcess.shape[0]):
     if x.size:
         xl = int(np.percentile(x, 10))
         xr = int(np.percentile(x, 90))
-        if xr - xl > 60:
+        if (xr - xl > 50) and (xr - xl < 275):
             centerPoints.append((int((x[0]+x[-1]) // 2), i))
-cv.polylines(img, [np.array(centerPoints, np.int32)], False, (0, 0, 255), 2)
+#cv.polylines(img, [np.array(centerPoints, np.int32)], False, (0, 0, 255), 2)
+cv.line(img, centerPoints[0], centerPoints[-1], (0, 255, 0), 2)
 
 cv.imshow("Curved Lines Detection", img)
 cv.waitKey(0)
