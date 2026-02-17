8ced18f5eaec8c8534bddf774cb2f3ebbd7b4890
pl1016814 <pl1016814@ahschool.com>
2026-02-17 07:54:41 -0500

    Update version and enhance line drawing functionality
    Updated version number and added functionality to draw lines between center points in increments of 25.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#"Curved Lines Detection" - Constantine Munoz P2, V1.0.0
+#"Curved Lines Detection" - Constantine Munoz P2, V1.0.8
 #Process of code:
 #Import libraries -> resize image to reduce lag -> grayscale + gaussian blur ->
 #Run Hough -> Find largest out of circles -> Draw the actual circle -> Show entire image
@@ -10,9 +10,19 @@ import numpy as np
 img = cv.imread("Screenshot2.png")
 img = cv.resize(img, (400, 200), interpolation=cv.INTER_AREA)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#gaussian blur as stated in documentation doesn't work for my soda can, idk why
 gray = cv.GaussianBlur(img, (9, 9), 2.5)
 cannyProcess = cv.Canny(gray, 135, 145)
+
+num, labels, stats, _ = cv.connectedComponentsWithStats(cannyProcess, connectivity=8)
+H = cannyProcess.shape[0]
+clean = np.zeros_like(cannyProcess)
+minHeight = np.zeros_like(cannyProcess)
+minHeight = int(0.35 * H)
+for i in range(1, num):
+    x, y, w, h, area = stats[i]
+    if h > minHeight:
+        clean[labels == i] = 255
+cannyProcess = clean
 
 centerPoints = []
 for i in range(cannyProcess.shape[0]):
@@ -22,8 +32,9 @@ for i in range(cannyProcess.shape[0]):
         xr = int(np.percentile(x, 90))
         if (xr - xl > 50) and (xr - xl < 275):
             centerPoints.append((int((x[0]+x[-1]) // 2), i))
-#cv.polylines(img, [np.array(centerPoints, np.int32)], False, (0, 0, 255), 2)
-cv.line(img, centerPoints[0], centerPoints[-1], (0, 255, 0), 2)
+
+for i in range(0, len(centerPoints) - 25, 25):
+    cv.line(img, centerPoints[i], centerPoints[i+25], (0, 255, 0), 2)
 
 cv.imshow("Curved Lines Detection", img)
 cv.waitKey(0)
